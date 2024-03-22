a007fdc806c6a95c015053e5c48737cdbc9ce875
Moath Alkhelifi <malkhelifi@rize.sa>
2024-03-22 21:31:02 +0300

    Added bloc with a little implementation

--- lib/features/number_trivia/presentation/number_trivia_bloc.dart
+++ lib/features/number_trivia/presentation/number_trivia_bloc.dart
@@ -1,15 +1,69 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 
+import '../../../core/error/failures.dart';
+import '../../../core/util/input_converter.dart';
 import '../domain/entities/number_trivia.dart';
+import '../domain/usecases/get_concrete_number_trivia.dart';
+import '../domain/usecases/get_random_number_trivia.dart';
+import 'number_trivia_event.dart';
 
-part 'number_trivia_event.dart';
+//part 'number_trivia_event.dart';
 part 'number_trivia_state.dart';
 
+const String serverFailureMessage = 'Server Failure';
+const String cacheFailureMessage = 'Cache Failure';
+const String invalidInput =
+    'Invalid Input - The number must be positive integer or zero';
+
 class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
-  NumberTriviaBloc() : super(Empty()) {
-    on<NumberTriviaEvent>((event, emit) {
-      // TODO: implement event handler
+  final GetConcreteNumberTrivia getConcreteNumberTrivia;
+  final GetRandomNumberTrivia getRandomNumberTrivia;
+  final InputConverter inputConverter;
+
+  NumberTriviaBloc({
+    required this.getConcreteNumberTrivia,
+    required this.getRandomNumberTrivia,
+    required this.inputConverter,
+  }) : super(Empty()) {
+    on<NumberTriviaEvent>((event, emit) async {
+      if (event is GetTriviaForConcreteNumber) {
+        final inputEither =
+            inputConverter.stringToUnsignedInteger(event.numberString);
+
+        inputEither.fold(
+            // If it was a Failure it will yield an Error
+            (l) => (failure) async* {
+                  yield Error(error: invalidInput);
+                },
+            (r) => (integer) async* {
+                  // This is a [Higher order function] which takes a function as an argument
+                  //
+                  // or returns a function
+                  yield Loading();
+
+                  final failureOrTrivia =
+                      await getConcreteNumberTrivia(Params(number: integer));
+
+                  yield failureOrTrivia.fold(
+                      (l) => Error(error: _mapFailureToMessage(l)),
+                      (r) => (trivia) {
+                            return Loaded(trivia: trivia);
+                          });
+                });
+      }
     });
+  }
+
+  // Instead of using ternary operator to decide which failure message to provide.
+  String _mapFailureToMessage(Failure failure) {
+    switch (failure.runtimeType) {
+      case serverFailureMessage:
+        return serverFailureMessage;
+      case cacheFailureMessage:
+        return cacheFailureMessage;
+      default:
+        return 'Unexpected Error';
+    }
   }
 }
--- lib/features/number_trivia/presentation/number_trivia_event.dart
+++ lib/features/number_trivia/presentation/number_trivia_event.dart
@@ -1,13 +1,19 @@
-part of 'number_trivia_bloc.dart';
+//part of 'number_trivia_bloc.dart';
+
+import 'package:equatable/equatable.dart';
 
 sealed class NumberTriviaEvent extends Equatable {
   const NumberTriviaEvent();
 }
 
-class getTriviaForConcreteNumber extends NumberTriviaEvent {
+// There is two events that will happen in the app
+//
+// Getting the Concrete Number
+// And getting the Random Number
+class GetTriviaForConcreteNumber extends NumberTriviaEvent {
   final String numberString;
 
-  getTriviaForConcreteNumber(this.numberString);
+  const GetTriviaForConcreteNumber(this.numberString);
 
   @override
   // TODO: implement props
@@ -16,7 +22,7 @@ class getTriviaForConcreteNumber extends NumberTriviaEvent {
   }
 }
 
-class getTrivialForRandomNumber extends NumberTriviaEvent {
+class GetTrivialForRandomNumber extends NumberTriviaEvent {
   @override
   // TODO: implement props
   List<Object?> get props => throw UnimplementedError();
